0d1a844813c40c5f80245db8d19df5f7dfc642b4
Gabo Martinez <gabmart02@gmail.com>
2016-05-12 13:30:53 -0400

    validating user fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,15 +26,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      flash[:notice] = "You signed up successfully"
+      flash[:color]= "valid"
+    else
+      flash[:notice] = "Form is invalid"
+      flash[:color]= "invalid"
+      render "new"
     end
+
+    
+
+
   end
 
   # PATCH/PUT /users/1
@@ -69,6 +72,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :first_name, :last_name, :dob, :country, :address1, :address2, :region, :postal_code, :tel, :email, :password_digest)
+      params.require(:user).permit(:username, :first_name, :last_name, :dob, :country, :address1, :address2, :region, :postal_code, :tel, :email, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
 	has_secure_password
-	
+	validates :first_name, :last_name, :username, :dob, :country, :address1, :region, :tel, :email, :password_confirmation, presence: true
 end

dde74db1d2a009d66a5f17389252a44bf0398280
Gabo Martinez <gabmart02@gmail.com>
2016-05-12 15:27:31 -0400

    adding date validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	validates :first_name, :last_name, :username, :dob, :country, :address1, :region, :tel, :email, :password_confirmation, presence: true
+	validates :email, uniqueness: true, format: { with: /\A[^@\s]+@([^@.\s]+\.)*[^@.\s]+\z/ }
+	validates :dob, :date => { on_or_after: :birth_date_first, on_or_before: :birth_date_last }
+
+
+
+
+	def self.birth_date_last
+		18.years.ago
+	end
+
+	def self.birth_date_first
+		118.years.ago
+	end
+
 end

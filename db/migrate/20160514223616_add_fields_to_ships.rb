f340b0f04df71dd6dc213d9a8aff4f6950406903
Susan Davies <sdav9375@gmail.com>
2016-05-14 18:55:57 -0400

    added & modified fields to ship table

---
+++ db/migrate/20160514223616_add_fields_to_ships.rb
@@ -0,0 +1,12 @@
+class AddFieldsToShips < ActiveRecord::Migration
+  def change
+    change_column :ships, :tonnage, :integer
+    change_column :ships, :max_beam, :integer
+    remove_column :ships, :length
+    remove_column :ships, :activities
+    add_column :ships, :length_in_feet, :integer
+    add_column :ships, :guest_capacity, :integer
+    add_column :ships, :onboard_crew, :integer
+    add_column :ships, :description, :text
+  end
+end

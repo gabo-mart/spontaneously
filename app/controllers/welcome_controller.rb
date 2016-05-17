aad050f87da563d106545f0d5141ce481760b310
Susan Davies <sdav9375@gmail.com>
2016-05-17 16:39:58 -0400

    fix package error

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @packages = Package.all
   end
   def show
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Ship.create([{
+  name: "Oasis of the Seas",
+  tonnage: 23000,
+  max_beam: 459,
+  type: nil,
+  other: 'Fun Ship',
+  vendor_id: '2',
+  length_in_feet: 2300,
+  guest_capacity: 4399,
+  onboard_crew: 909,
+  description: 'A really fun ship',
+  avatar: 'example.png'
+    },
+  {
+  name: "Oasis of the Seas",
+  tonnage: 23000,
+  max_beam: 459,
+  type: nil,
+  other: 'Fun Ship',
+  vendor_id: '2',
+  length_in_feet: 2300,
+  guest_capacity: 4399,
+  onboard_crew: 909,
+  description: 'A really fun ship',
+  avatar: 'example.png'
+    }])
+Add Comment C

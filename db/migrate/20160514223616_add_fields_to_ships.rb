f502eb8e2e64054767b0a77689915d369b3c6654
samcjsmith <samcjsmith@gmail.com>
2016-05-18 17:40:00 -0400

    Changed tonnage from string to interger

--- db/migrate/20160514223616_add_fields_to_ships.rb
+++ db/migrate/20160514223616_add_fields_to_ships.rb
@@ -1,7 +1,5 @@
 class AddFieldsToShips < ActiveRecord::Migration
   def change
-    change_column :ships, :tonnage, :integer
-    change_column :ships, :max_beam, :integer
     remove_column :ships, :length
     remove_column :ships, :activities
     add_column :ships, :length_in_feet, :integer

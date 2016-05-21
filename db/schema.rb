43ee4c7d155b30b9496c3e8d0f10679b99efc703
Susan Davies <sdav9375@gmail.com>
2016-05-21 11:59:31 -0400

    icon link and button added to cards front page

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520234834) do
+ActiveRecord::Schema.define(version: 20160521153424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "packages", force: :cascade do |t|
     t.string   "itinerary"
-    t.string   "ship"
     t.string   "duration"
     t.decimal  "price"
     t.integer  "rooms"

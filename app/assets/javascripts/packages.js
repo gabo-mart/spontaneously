521284f4e18728b26ef1fa9d898fa035ee0f7aab
Gabo Martinez <gabmart02@gmail.com>
2016-05-21 14:30:04 -0400

    fixing package _form

--- app/assets/javascripts/packages.js
+++ app/assets/javascripts/packages.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
  			$('select').material_select();
  		});
@@ -5,3 +6,4 @@ $(document).ready(function() {
 $(document).ready(function(){
       $('.slider').slider({full_width: true});
     });
+

61b9b660f1214445a03a1b9a49e59cdb90fcd275
samcjsmith <samcjsmith@gmail.com>
2016-05-22 15:40:20 -0400

    UDPATE ENV VAR

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.root = Rails.root.join('tmp')
-  config.cache_dir = 'carrierwave' 
+  config.cache_dir = 'carrierwave'
 
   if Rails.env.test? || Rails.env.cucumber?
     config.storage = :file
@@ -11,12 +11,13 @@ CarrierWave.configure do |config|
   end
 
   config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV['S3_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET'],
-    region:                ENV['S3_REGION']
+    # Configuration for Amazon S3
+    :provider              => 'AWS',
+    :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+    :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+    :region                => ENV['S3_REGION']
   }
-  config.fog_directory    = 'spontaneously'
+  config.fog_directory     =  ENV['S3_BUCKET']
 
 
   config.cache_dir = "#{Rails.root}/tmp/uploads"
